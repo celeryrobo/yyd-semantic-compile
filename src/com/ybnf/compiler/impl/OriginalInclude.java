e4708ac29950a25f219b3cc587a9c55ccefb2171
celery <liuqing@yydrobo.com>
2017-11-06 21:31:30 +0800

    commit code

--- src/com/ybnf/compiler/impl/OriginalInclude.java
+++ src/com/ybnf/compiler/impl/OriginalInclude.java
@@ -14,9 +14,10 @@ public class OriginalInclude extends Include {
 
 	@Override
 	public String readContent() throws Exception {
-		StringBuilder sb = new StringBuilder("<_yyd_han_> = #'\\p{script=Han}'\n");
-		sb.append("<_yyd_digital_> = #'\\d'\n<_yyd_num_> = _yyd_digital_+\n<_yyd_string_> = _yyd_wstr_|_yyd_str_");
-		sb.append("<_yyd_char_> = #'\\w'\n<_yyd_wstr_> = _yyd_han_+\n<_yyd_str_> = _yyd_char_+\n");
+		StringBuilder sb = new StringBuilder();
+		sb.append("<_yyd_han_> = #'\\p{script=Han}'\n<_yyd_char_> = #'\\w'\n<_yyd_ch_> = _yyd_han_|_yyd_char_\n");
+		sb.append("<_yyd_wstr_> = _yyd_han_+\n<_yyd_str_> = _yyd_char_+\n<_yyd_string_> = _yyd_ch_+\n");
+		sb.append("<_yyd_digital_> = #'\\d'\n<_yyd_num_> = _yyd_digital_+\n");
 		return sb.toString();
 	}
 
