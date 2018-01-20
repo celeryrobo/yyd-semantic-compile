b5fd4a1271d6583b51c1660442b49f67011bd235
celeryrobo <liuqing@yydrobo.com>
2018-01-20 15:43:32 +0800

    新增加减乘除符号

--- src/com/ybnf/compiler/impl/OriginalInclude.java
+++ src/com/ybnf/compiler/impl/OriginalInclude.java
@@ -19,6 +19,7 @@ public class OriginalInclude extends Include {
 		sb.append("<_yyd_wstr_> = _yyd_han_+\n<_yyd_str_> = _yyd_char_+\n<_yyd_string_> = _yyd_ch_+\n");
 		sb.append("<_yyd_digital_> = #'\\d'\n<_yyd_num_> = _yyd_digital_+\n");
 		sb.append("<_yyd_lxkh_> = '('|'（'\n<_yyd_rxkh_> = ')'|'）'\n<_yyd_lzkh_> = '['|'【'\n<_yyd_rzkh_> = ']'|'】'\n<_yyd_ldkh_> = '{'\n<_yyd_rdkh_> = '}'\n");
+		sb.append("<_yyd_plus_> = '+'\n<_yyd_subtract_> = '-'\n<_yyd_multi_> = '*'|'×'\n<_yyd_division_> = '/'|'÷'\n");
 		return sb.toString();
 	}
 
