06387903551dd513e51fb7150ecb2e83bbc7fa14
celeryrobo <liuqing@yydrobo.com>
2018-03-06 19:06:42 +0800

    commit code

--- src/com/ybnf/compiler/impl/LuceneCompiler.java
+++ src/com/ybnf/compiler/impl/LuceneCompiler.java
@@ -1,16 +1,65 @@
 package com.ybnf.compiler.impl;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.lucene.search.Query;
+
 import com.ybnf.compiler.ICompiler;
 import com.ybnf.compiler.beans.YbnfCompileResult;
+import com.ybnf.compiler.lucene.IndexReaderService;
+import com.ybnf.compiler.lucene.IndexWriterService;
+import com.ybnf.compiler.lucene.SemanticIntent;
+import com.ybnf.compiler.lucene.SemanticSentence;
+import com.ybnf.compiler.lucene.SemanticService;
+import com.ybnf.compiler.lucene.TemplateEntity;
 import com.ybnf.semantic.SemanticCallable;
 
 public class LuceneCompiler implements ICompiler {
-	public LuceneCompiler(){
+	private static Map<String, SemanticService> SERVICES = new HashMap<>();
+	private SemanticService semanticService = null;
+
+	public LuceneCompiler(Map<String, Map<String, List<String>>> sceneIntentTemplates) throws Exception {
+		try (IndexWriterService writerService = new IndexWriterService()) {
+			for (Entry<String, Map<String, List<String>>> sceneIntentTemplate : sceneIntentTemplates.entrySet()) {
+				String sceneName = sceneIntentTemplate.getKey();
+				SemanticService service = new SemanticService(sceneName);
+				for (Entry<String, List<String>> intentTemplate : sceneIntentTemplate.getValue().entrySet()) {
+					String intentName = intentTemplate.getKey();
+					SemanticIntent intent = service.buildIntent(intentName);
+					for (String template : intentTemplate.getValue()) {
+						intent.addTemplate(template);
+					}
+				}
+				writerService.initSemanticService(service);
+				SERVICES.put(sceneName, service);
+			}
+		}
+	}
+
+	public LuceneCompiler service(String service) throws Exception {
+		semanticService = SERVICES.get(service);
+		if (semanticService == null) {
+			throw new Exception("场景:" + service + "，不存在！");
+		}
+		return this;
 	}
 
 	@Override
 	public YbnfCompileResult compile(String text) throws Exception {
-		return null;
+		SemanticSentence sentence = semanticService.buildSentence(text);
+		Query query = sentence.buildQuery("template");
+		TemplateEntity entity = null;
+		try (IndexReaderService readerService = new IndexReaderService()) {
+			entity = readerService.search(query);
+		}
+		YbnfCompileResult result = null;
+		if (entity != null) {
+			result = sentence.intent(entity.getIntent()).compile(entity.getTemplate());
+		}
+		return result;
 	}
 
 	@Override
