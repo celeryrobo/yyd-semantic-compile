939d3186e7d551d2ea85e3f1998b8604e33ae48e
celeryrobo <liuqing@yydrobo.com>
2018-11-21 17:20:51 +0800

    移除索引创建时的场景打印逻辑

--- src/com/ybnf/compiler/lucene/IndexWriterService.java
+++ src/com/ybnf/compiler/lucene/IndexWriterService.java
@@ -42,7 +42,7 @@ public class IndexWriterService extends LuceneService {
 	}
 
 	public void addTemplate(String service, String intent, Template template) throws Exception {
-		if("flyGame".equals(service)) {
+		if("moonGame".equals(service)) {
 			System.out.println(template);
 		}
 		addTemplateEntity(new TemplateEntity(0, 0, service, intent, template.getTemplate()));
