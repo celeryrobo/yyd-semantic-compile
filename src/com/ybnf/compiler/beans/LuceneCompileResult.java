b15b43bc7a4572e25b1268a861e8d9ae4c6b7707
sunyibin <sunyibin@yydrobo.com>
2019-05-27 14:25:42 +0800

    添加getAppId方法

--- src/com/ybnf/compiler/beans/LuceneCompileResult.java
+++ src/com/ybnf/compiler/beans/LuceneCompileResult.java
@@ -4,14 +4,24 @@ import com.ybnf.compiler.lucene.TemplateEntity;
 
 public class LuceneCompileResult extends YbnfCompileResult {
 	private TemplateEntity templateEntity;
-
+	private Integer appId;
 	public LuceneCompileResult(YbnfCompileResult ybnfCompileResult, TemplateEntity templateEntity) {
 		super(ybnfCompileResult.getText(), ybnfCompileResult.getVersion(), ybnfCompileResult.getCharset(),
 				ybnfCompileResult.getService(), ybnfCompileResult.getObjects(), ybnfCompileResult.getSlots());
 		this.templateEntity = templateEntity;
 	}
+	
+	public LuceneCompileResult(YbnfCompileResult ybnfCompileResult, Integer appId) {
+		super(ybnfCompileResult.getText(), ybnfCompileResult.getVersion(), ybnfCompileResult.getCharset(),
+				ybnfCompileResult.getService(), ybnfCompileResult.getObjects(), ybnfCompileResult.getSlots());
+		this.appId=appId;
+	}
 
 	public TemplateEntity getTemplateEntity() {
 		return templateEntity;
+	}
+
+	public Integer getAppId() {
+		return appId;
 	}
 }
